93ed5200d390a3e9161d01218e25a5f17586e70a
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-12-04 17:44:14 -0600

    finished MVP project

--- src/adv.py
+++ src/adv.py
@@ -1,8 +1,8 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
-room = {
+rooms = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
@@ -24,23 +24,79 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+rooms['outside'].n_to = rooms['foyer']
+rooms['foyer'].s_to = rooms['outside']
+rooms['foyer'].n_to = rooms['overlook']
+rooms['foyer'].e_to = rooms['narrow']
+rooms['overlook'].s_to = rooms['foyer']
+rooms['narrow'].w_to = rooms['foyer']
+rooms['narrow'].n_to = rooms['treasure']
+rooms['treasure'].s_to = rooms['narrow']
 
 #
 # Main
 #
+first_player = Player("BraveHeart", rooms['outside'])
 
-# Make a new player object that is currently in the 'outside' room.
+def advance_room(player, player_input):
+    if player_input == 'n':
+        player.current_room = player.current_room.n_to
+    elif player_input == 's':
+        player.current_room = player.current_room.s_to
+    elif player_input == 'e':
+        player.current_room = player.current_room.e_to
+    elif player_input == 'w':
+        player.current_room = player.current_room.w_to
+    else:
+        return False
+
+def check_bad_input(player, player_input):
+    foyer_possible_inputs = ['s', 'n', 'e']
+    narrow_possible_inputs = ['w','n']
+    
+    if player.current_room.name == 'Outside Cave Entrance' and player_input == 'n':
+        return True
+    elif player.current_room.name == 'Foyer' and player_input in foyer_possible_inputs:
+        return True
+    elif player.current_room.name == 'Grand Overlook' and player_input == 's':
+        return True
+    elif player.current_room.name == 'Narrow Passage' and player_input in narrow_possible_inputs:
+        return True
+    elif player.current_room.name == 'Treasure Chamber' and player_input == 's':
+        return True
+    else:
+        return False
+
+possible_inputs = ['n', 's', 'w', 'e', 'q']
+
+print('Enter "n" for North, "s" for South, "e" for East, "w" for West and "q" to quit the game.')
+while True:
+    print(first_player)
+    player_input = input("-> ")
+
+    if player_input in possible_inputs:
+        result = check_bad_input(first_player, player_input)
+        if player_input == 'q':
+            print(f"Goodbye {first_player.name}")
+            break
+        if result:
+            advance_room(first_player, player_input)
+        else:
+            print('You shall not pass! Read the description carefully to choose your path!')
+    else:
+        print('Invalid command! Please enter "n" for North, "s" for South, "e" for East, "w" for West and "q" to quit the game.')
+
+# def main():
+# Make a new player object that is currently in the 'outside' room.\
+#     player_input = input("Enter n for advancing to North:")
+#     if player_input:
+#         advancing_room(first_player, player_input)
+# main()
+
+
 
 # Write a loop that:
-#
+#   
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+    def __str__(self):
+        return f'{self.name}, you are at \'{self.current_room.name}\' and {self.current_room.description}!'
